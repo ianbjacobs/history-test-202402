89488cf61c81b68b51dd07710b8dd0a4a5ff919d
Ian Jacobs <ij@w3.org>
2024-02-07 22:31:08 -0600

    fix

--- assets/members.js
+++ assets/members.js
@@ -61,7 +61,7 @@ function $$(selector, context = document) {
 }
 
 function sortByStartDate (a, b) {
-    return a.start - b.start
+    return new Date(a.start) - new Date(b.start);
 }
 
 document.head.insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">`)
